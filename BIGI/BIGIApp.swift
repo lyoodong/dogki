e61a6c0750984e1286c53e5ac1ffc7b605f52fb0
lyoodong <dwryu3079@naver.com>
2025-10-09 19:11:16 +0900

    Initial Commit

---
+++ BIGI/BIGIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIGIApp.swift
+//  BIGI
+//
+//  Created by Dongwan Ryoo on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BIGIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
