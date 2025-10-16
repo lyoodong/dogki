3d77854f4f8c7f2fb57f865c3d3f64474f93b2cf
lyoodong <dwryu3079@naver.com>
2025-10-16 15:58:33 +0900

    [add] admob native ads setting

--- BIGI/AppDelegate.swift
+++ BIGI/AppDelegate.swift
@@ -1,10 +1,15 @@
 import SwiftUI
 import FirebaseCore
+import GoogleMobileAds
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        MobileAds.shared.start()
+        MobileAds.shared.requestConfiguration.testDeviceIdentifiers = [ "410a69826223a2ad58ebf914fb5e8c8e" ]
+
+        return true
+    }
 }
---
+++ BIGI/Source/View/Test/TestListAdsView.swift
@@ -0,0 +1,8 @@
+//
+//  TestListAdsView.swift
+//  BIGI
+//
+//  Created by Dongwan Ryoo on 10/16/25.
+//
+
+import Foundation
